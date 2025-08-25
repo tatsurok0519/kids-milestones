d9dd999283245a9c572f6f7b1ccaef0416ca70cb
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-25 17:32:51 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :basic_auth_applicable?
+
+  private
+
+  def basic_auth_applicable?
+    Rails.env.production? &&
+      ENV["BASIC_AUTH_USER"].present? &&
+      ENV["BASIC_AUTH_PASSWORD"].present? &&
+      request.path != "/up" # ヘルスチェックは除外
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic("Restricted") do |user, pass|
+      ActiveSupport::SecurityUtils.secure_compare(user.to_s,  ENV.fetch("BASIC_AUTH_USER")) &&
+      ActiveSupport::SecurityUtils.secure_compare(pass.to_s,  ENV.fetch("BASIC_AUTH_PASSWORD"))
+    end
+  end
 end
