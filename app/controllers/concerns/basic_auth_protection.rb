4331ab2beda6aa8ef7911d4c73425344cc59d2e3
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-25 18:59:35 +0900

    Basic認証その２

---
+++ app/controllers/concerns/basic_auth_protection.rb
@@ -0,0 +1,23 @@
+module BasicAuthProtection
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :basic_auth, if: :basic_auth_applicable?
+  end
+
+  private
+
+  def basic_auth_applicable?
+    Rails.env.production? &&
+      ENV["BASIC_AUTH_USER"].present? &&
+      ENV["BASIC_AUTH_PASSWORD"].present? &&
+      request.path != "/up" # ヘルスチェックは除外
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic("Restricted") do |user, pass|
+      ActiveSupport::SecurityUtils.secure_compare(user.to_s, ENV.fetch("BASIC_AUTH_USER")) &&
+      ActiveSupport::SecurityUtils.secure_compare(pass.to_s, ENV.fetch("BASIC_AUTH_PASSWORD"))
+    end
+  end
+end
