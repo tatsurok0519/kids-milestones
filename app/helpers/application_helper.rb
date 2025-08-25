75698a69d07547df4e15a0c929c774d9f736bdb6
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-25 21:07:10 +0900

    HTML修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def display_user_name
+    return "ゲスト" unless user_signed_in?
+
+    if current_user.respond_to?(:name) && current_user.name.present?
+      current_user.name
+    else
+      # nameカラムが無い/未入力なら、メールの@前を仮の表示名に
+      current_user.email.to_s.split("@").first
+    end
+  end
 end
