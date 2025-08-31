7d04a45d80fe75d0fe8b73cf92720f4d3dafcc77
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-08-31 20:33:28 +0900

    メダル・トロフィー演出本番その３

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,15 +1,14 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-# ↓重複・混乱の元なので削除
+# ↓は削除（重複）
 # pin "@hotwired/turbo", to: "@hotwired--turbo.js"
 
 pin "@hotwired/stimulus",          to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading",  to: "stimulus-loading.js", preload: true
 pin "@rails/activestorage",        to: "activestorage.esm.js"
 
-# Stimulus を使う場合（使ってなくても害はありません）
-pin_all_from "app/javascript/controllers", under: "controllers"
-
-# 既存のアプリスクリプト
 pin "chat_consult",  to: "chat_consult.js"
 pin "photo_preview", to: "photo_preview.js"
+
+# Stimulus を使うなら
+pin_all_from "app/javascript/controllers", under: "controllers"
