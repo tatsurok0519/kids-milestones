e0cf2c2ba0153029670d2cb75b5eec491af45330
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-09-03 06:42:33 +0900

    本番ログインできない問題その２

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
-//= link_tree ../builds            // ← Tailwind の生成物（application.css など）だけ読む
-//= link_tree ../../javascript .js // ← Importmap で配信するJSをSprocketsに認識させる
+//= link_tree ../builds
+//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
